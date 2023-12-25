b188b3abd255a41b1641753a9ded6a173e57b480
likanstha <likan.stha@gmail.com>
2023-12-25 15:52:23 +1100

    server config and db connect

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+
+const dbConnect = require('./config/database');
+
+dotenv.config({
+  path: 'backend/config/config.env',
+});
 
 const PORT = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send('This is from backend');
 });
+
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
